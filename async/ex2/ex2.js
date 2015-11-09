40ba82c6ff97d0e5146e0552533c08da7910078d
Mark Thomas <markthethomas@gmail.com>
2015-11-09 09:46:25 -0800

    finish and add ex2

--- async/ex2/ex2.js
+++ async/ex2/ex2.js
@@ -20,8 +20,31 @@ function output(text) {
 // **************************************
 
 function getFile(file) {
-	// what do we do here?
+	var resp;
+
+	fakeAjax(file,function(text){
+		if (!resp) resp = text;
+		else resp(text);
+	});
+
+	return function th(cb) {
+		if (resp) cb(resp);
+		else resp = cb;
+	};
 }
 
 // request all files at once in "parallel"
-// ???
+var th1 = getFile("file1");
+var th2 = getFile("file2");
+var th3 = getFile("file3");
+
+th1(function ready(text){
+	output(text);
+	th2(function ready(text){
+		output(text);
+		th3(function ready(text){
+			output(text);
+			output("Complete!");
+		});
+	});
+});
